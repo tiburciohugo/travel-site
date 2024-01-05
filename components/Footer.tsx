e09cea5cd1eb826ac6314cae24f88b0570e42611
Hugo Tiburcio <hugotiburcio@outlook.com>
2024-01-05 15:04:47 -0300

    fix: update files and imports

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,4 @@
-import {
-  FOOTER_CONTACT_INFO,
-  FOOTER_LINKS,
-  SOCIALS,
-} from "@/constants/constants.index";
+import { FOOTER_CONTACT_INFO, FOOTER_LINKS, SOCIALS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -12,30 +8,16 @@ export default function Footer() {
     <footer className="flexCenter mb-24">
       <div className="padding-container max-container flex w-full flex-col gap-14">
         <div className="flex flex-col items-start justify-center gap-[10%] md:flex-row">
-          <Link
-            href="/"
-            className="mb-10"
-          >
-            <Image
-              src="hilink-logo.svg"
-              alt="logo"
-              width={74}
-              height={29}
-            />
+          <Link href="/" className="mb-10">
+            <Image src="hilink-logo.svg" alt="logo" width={74} height={29} />
           </Link>
 
           <div className="flex flex-wrap gap-10 sm:justify-between md:flex-1">
             {FOOTER_LINKS.map((columns) => (
-              <FooterColumn
-                key={columns.title}
-                title={columns.title}
-              >
+              <FooterColumn key={columns.title} title={columns.title}>
                 <ul className="regular-14 flex flex-col gap-4 text-gray-30">
                   {columns.links.map((link) => (
-                    <Link
-                      href="/"
-                      key={link}
-                    >
+                    <Link href="/" key={link}>
                       {link}
                     </Link>
                   ))}
@@ -64,16 +46,8 @@ export default function Footer() {
               <FooterColumn title={SOCIALS.title}>
                 <ul className="regular-14 flex gap-4 text-gray-30">
                   {SOCIALS.links.map((link) => (
-                    <Link
-                      href="/"
-                      key={link}
-                    >
-                      <Image
-                        src={link}
-                        alt="logo"
-                        width={24}
-                        height={24}
-                      />
+                    <Link href="/" key={link}>
+                      <Image src={link} alt="logo" width={24} height={24} />
                     </Link>
                   ))}
                 </ul>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { NAV_LINKS } from "@/constants/constants.index";
+import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -7,12 +7,7 @@ export default function Navbar() {
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
-        <Image
-          alt="logo"
-          height={29}
-          src="/hilink-logo.svg"
-          width={74}
-        />
+        <Image alt="logo" height={29} src="/hilink-logo.svg" width={74} />
       </Link>
 
       <ul className="hidden h-full gap-12 lg:flex">
@@ -20,7 +15,7 @@ export default function Navbar() {
           <Link
             href={link.href}
             key={link.key}
-            className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold"
+            className="regular-16 flexCenter cursor-pointer pb-1.5 text-gray-50 transition-all hover:font-bold"
           >
             {link.label}
           </Link>
